b1e900277c6d29b8302c6afc6bd23bdd62b60997
Tempest <d7oomalshebel@gmail.com>
2026-05-24 21:44:13 +0300

    use correct robloxmemoryapi version

--- requirements.bat
+++ requirements.bat
@@ -2,7 +2,7 @@
 echo Installing packages...
 echo.
 
-pip install pyautogui colorama dearpygui robloxmemoryapi
+pip install pyautogui colorama dearpygui robloxmemoryapi==0.2.9
 
 echo.
 echo Done. You can close this window.
